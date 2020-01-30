d38b357519e57ae7338e709d1953de4084d3cd4b
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-01-30 11:21:39 +0000

    .

--- app/src/components/StudentForm.js
+++ app/src/components/StudentForm.js
@@ -1,89 +1,82 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import axios from 'axios';
 
 export default function StudentForm() {
-  const [studentForm, setStudentForm] = useState(initialState);
+  function handleSubmit(values, actions) {
+    console.log(values);
 
-  function onFormChange(e) {
-    // console.log(e.target.name);
-    if (e.target.value === e.target.value.toLowerCase()) {
-      setStudentForm({
-        ...studentForm,
-        [e.target.name]:
-          e.target.type === 'checkbox' ? e.target.checked : e.target.value
-      });
-    }
-  }
-
-  function handleSubmit(e){
-    e.preventDefault();
-    console.log(studentForm);
-    // setStudentForm(initialState);
     axios
-    .post('https://reqres.in/api/users/', studentForm)
-    .then(res => {
-      console.log(res);
-      setStudentForm(initialState);
-    })
-    .catch(e => console.log(e))
-    .finally(() => {
-      console.log('Axios request finished.');
-    });
+      .post('https://reqres.in/api/users/', values)
+      .then(res => {
+        console.log(res.data);
+        actions.resetForm();
+      })
+      .catch(e => console.log(e))
   }
-
   return (
     <div className="StudentForm">
       <h1>Lambda Student Form</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="student_first_name">First Name</label>
-        <input
-          type="text"
-          id="student_first_name"
-          name="first_name"
-          placeholder="Enter your first name here"
-          onChange={onFormChange}
-          value={studentForm.firstName}
-        />
-        <label htmlFor="student_last_name">Last Name</label>
-        <input
-          type="text"
-          id="student_last_name"
-          name="last_name"
-          placeholder="Enter your last name here"
-          onChange={onFormChange}
-          value={studentForm.last_name}
-        />
-        <label htmlFor="student_username">Username</label>
-        <input
-          type="text"
-          id="student_username"
-          name="username"
-          placeholder="Enter your username here"
-          onChange={onFormChange}
-          value={studentForm.username}
-        />
-        <label htmlFor="student_password">Password</label>
-        <input
-          type="password"
-          id="student_password"
-          name="password"
-          placeholder="Enter your password here"
-          onChange={onFormChange}
-          value={studentForm.password}
-        />
-        <label htmlFor="student_remember_pass">Remember password?</label>
-        <input
-          type="checkbox"
-          id="student_remember_pass"
-          name="remember_pass"
-          onChange={onFormChange}
-          checked={studentForm.remember_pass}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <Formik
+        onSubmit={handleSubmit}
+        initialValues={initialState}
+        validationSchema={validationSchema}
+      >
+        <Form>
+          <label htmlFor="student_first_name">First Name</label>
+          <Field
+            type="text"
+            id="student_first_name"
+            name="first_name"
+            placeholder="Enter your first name here"
+          />
+          <ErrorMessage name="first_name" component="div" className="error"/>
+          <label htmlFor="student_last_name">Last Name</label>
+          <Field
+            type="text"
+            id="student_last_name"
+            name="last_name"
+            placeholder="Enter your last name here"
+          />
+          <ErrorMessage name="last_name" component="div" className="error"/>
+          <label htmlFor="student_username">Username</label>
+          <Field
+            type="text"
+            id="student_username"
+            name="username"
+            placeholder="Enter your username here"
+          />
+          <ErrorMessage name="username" component="div" className="error"/>
+          <label htmlFor="student_password">Password</label>
+          <Field
+            type="password"
+            id="student_password"
+            name="password"
+            placeholder="Enter your password here"
+          />
+          <ErrorMessage name="password" component="div" className="error"/>
+          <label htmlFor="student_remember_pass">Remember password?</label>
+          <Field
+            type="checkbox"
+            id="student_remember_pass"
+            name="remember_pass"
+          />
+          <ErrorMessage name="remember_pass" component="div" className="error"/>
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
     </div>
   );
 }
+
+const validationSchema = Yup.object().shape({
+  first_name: Yup.string().required('Please enter your first name'),
+  last_name: Yup.string().required('Please enter your lastname name'),
+  username: Yup.string().required('Please enter a username'),
+  password: Yup.string().required('Please enter a password'),
+  remember_pass: Yup.boolean()
+});
 
 const initialState = {
   first_name: '',
