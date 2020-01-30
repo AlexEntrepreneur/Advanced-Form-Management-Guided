5b0c51f8b0c3109d5cde25fcb9cfd2d589a971e5
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-01-30 10:58:44 +0000

    .

--- app/src/components/TestingForms.js
+++ app/src/components/TestingForms.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from 'axios';
-import { Form, Formik, Field } from 'formik';
+import * as Yup from 'yup';
+import { Form, Formik, Field, ErrorMessage } from 'formik';
 
 export default function TestingForms() {
   function submitHandler(values, actions) {
@@ -23,6 +24,7 @@ export default function TestingForms() {
       <Formik
         onSubmit={submitHandler}
         initialValues={initialTestingFormValues}
+        validationSchema={validationSchema}
       >
         <Form>
           <label htmlFor="testform_username">Username</label>
@@ -31,22 +33,23 @@ export default function TestingForms() {
             id="testform_username"
             name="username"
             placeholder="Enter your username here"
-            required
           />
+          <ErrorMessage name="username" component="div" className="error"/>
           <label htmlFor="testform_password">Password</label>
           <Field
             type="password"
             id="testform_password"
             name="password"
             placeholder="Enter your password here"
-            required
           />
+          <ErrorMessage name="password" component="div" className="error"/>
           <label htmlFor="testform_remember_pass">Remember password?</label>
           <Field
             type="checkbox"
             id="testform_remember_pass"
             name="remember_pass"
           />
+          <ErrorMessage name="remember_pass" component="div" className="error"/>
           {/* <Field
             as="textarea"
             // id="testform_remember_pass"
@@ -57,19 +60,26 @@ export default function TestingForms() {
             as="select"
             name="account_type"
             id="testform_account_type"
-            required
           >
             <option value="">Select an option</option>
             <option value="gold">Gold Account</option>
             <option value="slvr">Silver Account</option>
             <option value="brnz">Bronze Account</option>
           </Field>
+          <ErrorMessage name="account_type" component="div" className="error"/>
           <button type="submit">Submit</button>
         </Form>
       </Formik>
     </div>
   );
 }
+
+const validationSchema = Yup.object().shape({
+  username: Yup.string().required('Please enter a username'),
+  password: Yup.string().required('Please enter a password'),
+  remember_pass: Yup.boolean(),
+  account_type: Yup.string().required('Please select an account type')
+});
 
 // Clearing the values in our form inputs
 const initialTestingFormValues = {
