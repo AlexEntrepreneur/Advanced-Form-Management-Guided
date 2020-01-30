515efcafe032e61bd14b9084b5af4e311bdfc11b
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-01-30 10:31:04 +0000

    .

--- app/src/components/TestingForms.js
+++ app/src/components/TestingForms.js
@@ -1,108 +1,78 @@
-import React, { useState } from "react";
+import React from "react";
 import axios from 'axios';
+import { Form, Formik, Field } from 'formik';
 
 export default function TestingForms() {
-  const [testingForm, setTestingForms] = useState(initialState);
-
-  function changeHandler(e) {
-    // console.log(e.target.name);
-    if (e.target.value.length < 18) {
-        setTestingForms({
-          ...testingForm,
-          [e.target.name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value
-        });
-      }
-  }
-
-  function submitHandler(e) {
-    e.preventDefault();
-
-    //To see the values on our event object we can persist it
-    e.persist();
-    // console.log(e);
-
+  function submitHandler(values, actions) {
+    console.log(values, actions);
     // Sending our form data to a server
-    if (!formInputIsEmpty()) {
-      axios
-        .post('https://reqres.in/api/users/', testingForm)
-        .then(res => {
-          console.log(res);
-          resetFormValues();
-        })
-        .catch(e => console.log(e))
-        .finally(() => {
-          console.log('Axios request finished.');
-        });
-    } else {
-      console.log('Form is incomplete!');
-    }
-  }
-
-  function formInputIsEmpty() {
-    return (
-      testingForm.username === '' ||
-      testingForm.password === '' ||
-      testingForm.account_type === ''
-    );
-  }
-
-  function resetFormValues() {
-    // Reset the form state
-    setTestingForms(initialState);
+    axios
+      .post('https://reqres.in/api/users/', values)
+      .then(res => {
+        console.log(res);
+        actions.resetForm();
+      })
+      .catch(e => console.log(e))
+      .finally(() => {
+        console.log('Axios request finished.');
+      });
   }
   
   return (
     <div className="TestingForms">
-      <form onSubmit={submitHandler}>
-        <label htmlFor="testform_username">Username</label>
-        <input
-          type="text"
-          id="testform_username"
-          name="username"
-          placeholder="Enter your username here"
-          onChange={changeHandler}
-          value={testingForm.username}
-          required
-        />
-        <label htmlFor="testform_password">Password</label>
-        <input
-          type="password"
-          id="testform_password"
-          name="password"
-          placeholder="Enter your password here"
-          onChange={changeHandler}
-          value={testingForm.password}
-          required
-        />
-        <label htmlFor="testform_remember_pass">Remember password?</label>
-        <input
-          type="checkbox"
-          id="testform_remember_pass"
-          name="remember_pass"
-          onChange={changeHandler}
-          checked={testingForm.remember_pass}
-        />
-        <label htmlFor="testform_account_type">Select An Account Type: </label>
-        <select
-          name="account_type"
-          id="testform_account_type"
-          onChange={changeHandler}
-          value={testingForm.account_type}
-          required
-        >
-          <option value="">Select an option</option>
-          <option value="gold">Gold Account</option>
-          <option value="slvr">Silver Account</option>
-          <option value="brnz">Bronze Account</option>
-        </select>
-        <button type="sybmit">Submit</button>
-      </form>
+      <Formik
+        onSubmit={submitHandler}
+        initialValues={initialTestingFormValues}
+      >
+        <Form>
+          <label htmlFor="testform_username">Username</label>
+          <Field
+            type="text"
+            id="testform_username"
+            name="username"
+            placeholder="Enter your username here"
+            required
+          />
+          <label htmlFor="testform_password">Password</label>
+          <Field
+            type="password"
+            id="testform_password"
+            name="password"
+            placeholder="Enter your password here"
+            required
+          />
+          <label htmlFor="testform_remember_pass">Remember password?</label>
+          <Field
+            type="checkbox"
+            id="testform_remember_pass"
+            name="remember_pass"
+          />
+          {/* <Field
+            as="textarea"
+            // id="testform_remember_pass"
+            // name="remember_pass"
+          /> */}
+          <label htmlFor="testform_account_type">Select An Account Type: </label>
+          <Field
+            as="select"
+            name="account_type"
+            id="testform_account_type"
+            required
+          >
+            <option value="">Select an option</option>
+            <option value="gold">Gold Account</option>
+            <option value="slvr">Silver Account</option>
+            <option value="brnz">Bronze Account</option>
+          </Field>
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
     </div>
   );
 }
 
 // Clearing the values in our form inputs
-const initialState = {
+const initialTestingFormValues = {
   username: '',
   password: '',
   remember_pass: false,
