ea0dce95e1479bbe2e522bb41fb4ad0fb1162f6e
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-01-30 01:14:21 +0000

    Add Formik Solution Code (Without HOC)

--- app/src/components/TestingForms.js
+++ app/src/components/TestingForms.js
@@ -1,80 +1,11 @@
 import React from 'react';
-import { withFormik, Form, Field, ErrorMessage } from "formik";
+import { Form, Field, ErrorMessage, Formik } from "formik";
 import * as Yup from 'yup';
 import axios from 'axios';
 
 
-function TestingForms(props) {
-  console.log(props);
-  
-  return (
-    <div className="TestingForms">
-      <Form>
-        <label htmlFor="testform_username">Username</label>
-        <ErrorMessage
-          name="username"
-          render={msg => <div className="error">{msg}</div>}
-        />
-        <Field
-          type="text"
-          name="username"
-          id="testform_username"
-          placeholder="Enter your username here"
-        />
-
-        <label htmlFor="testform_password">Password</label>
-        <ErrorMessage
-          name="password"
-          render={msg => <div className="error">{msg}</div>}
-        />
-        <Field
-          type="password"
-          name="password"
-          id="testform_password"
-          placeholder="Enter your password here"
-        />
-        <label htmlFor="testform_remember_pass">Remember password?</label>
-        <Field
-          type="checkbox"
-          name="remember_pass"
-          id="testform_remember_pass"
-        />
-        <label htmlFor="testform_account_type">Select an account type: </label>
-        <ErrorMessage
-          name="account_type"
-          render={msg => <div className="error">{msg}</div>}
-        />
-        <Field as="select" name="account_type" id="testform_account_type">
-          <option>Gold Account</option>
-          <option>Silver Account</option>
-          <option>Bronze Account</option>
-        </Field>
-        <button type="submit">Submit</button>
-      </Form>
-    </div>
-  );
-}
-
-const FormikTestingForms = withFormik({
-  mapPropsToValues({ username, password, remember_pass, account_type }) {
-    return {
-      username: username || "",
-      password: password || "",
-      remember_pass: remember_pass || false,
-      account_type: account_type || "Gold Account"
-    };
-  },
-
-  validationSchema: Yup.object().shape({
-    username: Yup.string().required("Please enter a username"),
-    password: Yup.string().required("Please enter a password"),
-    remember_pass: Yup.boolean(),
-    account_type: Yup.string()
-      .oneOf(["Gold Account", "Silver Account", "Bronze Account"])
-      .required("Please select an account type")
-  }),
-
-  handleSubmit(values, tools) {
+export default function TestingForms() {
+  function submitHandler(values, tools) {
     // console.log(args);
     
     axios
@@ -86,6 +17,77 @@ const FormikTestingForms = withFormik({
       })
       .catch(err => console.log(err.response));
   }
-})(TestingForms);
+  return (
+    <div className="TestingForms">
+      <Formik
+        validationSchema={validationSchema}
+        initialValues={initialTestingFormValues}
+        onSubmit={submitHandler}
+        render={() =>
+          <Form>
+            <label htmlFor="testform_username">Username</label>
+            <Field
+              type="text"
+              name="username"
+              id="testform_username"
+              placeholder="Enter your username here"
+            />
+            <ErrorMessage
+              name="username"
+              className="error"
+              component="div"
+            />
+            <label htmlFor="testform_password">Password</label>
+            <Field
+              type="password"
+              name="password"
+              id="testform_password"
+              placeholder="Enter your password here"
+            />
+            <ErrorMessage
+              name="password"
+              className="error"
+              component="div"
+            />
+            <label htmlFor="testform_remember_pass">Remember password?</label>
+            <Field
+              type="checkbox"
+              name="remember_pass"
+              id="testform_remember_pass"
+            />
+            <label htmlFor="testform_account_type">
+              Select an account type:{" "}
+            </label>
+            <Field as="select" name="account_type" id="testform_account_type">
+              <option>Gold Account</option>
+              <option>Silver Account</option>
+              <option>Bronze Account</option>
+            </Field>
+            <ErrorMessage
+              name="account_type"
+              className="error"
+              component="div"
+            />
+            <button type="submit">Submit</button>
+          </Form>
+        }
+      />
+    </div>
+  );
+};
 
-export default FormikTestingForms;
+const validationSchema = Yup.object().shape({
+  username: Yup.string().required("Please enter a username"),
+  password: Yup.string().required("Please enter a password"),
+  remember_pass: Yup.boolean(),
+  account_type: Yup.string()
+    .oneOf(["Gold Account", "Silver Account", "Bronze Account"])
+    .required("Please select an account type")
+});
+
+const initialTestingFormValues = {
+  username: '',
+  password: '',
+  remember_pass: false,
+  account_type: ''
+};
